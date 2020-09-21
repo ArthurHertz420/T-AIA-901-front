14e6ca50e6fed65cc78de98bff782778e3bf1c1a
Alexis <alexis.trousset@yahoo.fr>
2020-09-21 15:40:42 +0200

    :construction: Create Web app without api call

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="q-app">
+    <router-view />
+  </div>
+</template>
+<script>
+import Vue from 'vue'
+import AudioRecorder from 'vue-audio-recorder'
+import VueRecord from '@codekraft-studio/vue-record'
+export default {
+  name: 'App',
+  mounted () {
+    Vue.use(VueRecord)
+    Vue.use(AudioRecorder)
+  }
+}
+</script>
