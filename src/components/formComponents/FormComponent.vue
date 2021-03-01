6e2c71300f9338e0336766126cc91cf7298e7569
Aleksistr <44772759+Aleksistr@users.noreply.github.com>
2021-03-01 10:51:02 +0100

    Update FormComponent.vue

--- src/components/formComponents/FormComponent.vue
+++ src/components/formComponents/FormComponent.vue
@@ -1,36 +1,12 @@
 <template>
-  <q-card style="width: 100vw !important">
-    <q-tabs
-      v-model="tab"
-      dense
-      align="justify"
-      class="bg-grey-3 text-grey-7"
-      active-bg-color="primary"
-      active-color="white"
-    >
-      <q-tab name="write" :label="$t('tabs.write')"/>
-      <q-tab name="speak" :label="$t('tabs.speak')"/>
-    </q-tabs>
-    <q-tab-panels v-model="tab" animated>
-      <q-tab-panel name="write">
-        <!-- Call component to write request -->
-        <WriteComponent v-on:search="search($event)" v-on:cancel="closeForm()"/>
-      </q-tab-panel>
-      <q-tab-panel name="speak">
-        <!-- Call component to tell the resquest -->
-        <SpeakComponent v-on:search="search($event)" v-on:cancel="closeForm()"/>
-      </q-tab-panel>
-    </q-tab-panels>
-  </q-card>
+  <div><SpeakComponent v-on:search="search($event)" v-on:cancel="closeForm()"/></div>
 </template>
 
 <script>
-import WriteComponent from './WriteComponent'
 import SpeakComponent from './SpeakComponent'
 export default {
   name: 'FormComponent',
   components: {
-    WriteComponent,
     SpeakComponent
   },
   data () {
