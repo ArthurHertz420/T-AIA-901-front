8bb384014c7ac0e4d146769e507aaeaa799e0f82
Aleksistr <44772759+Aleksistr@users.noreply.github.com>
2021-03-01 10:27:02 +0100

    Update index .html

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,6 @@
 <template>
   <q-page class="flex flex-center">
+    <div>{{result}}</div>
     <q-footer class="flex flex-center" style="background-color: white;">
       <FormRequestComponent/>
     </q-footer>
@@ -10,8 +11,18 @@
 import FormRequestComponent from '../components/formComponents/FormRequestComponent'
 export default {
   name: 'PageIndex',
+  data () {
+    return {
+      result: ''
+    }
+  },
   components: {
     FormRequestComponent
+  },
+  created () {
+    this.$root.$on('result', event => {
+      this.result = event.data
+    })
   }
 }
 </script>
